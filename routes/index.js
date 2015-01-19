8bc0dcd3532eb7a633025b97ca7ba834439cd26c
Deary Hudson Jr <hudson.deary@gmail.com>
2015-01-18 23:53:13 -0600

    login

--- routes/index.js
+++ routes/index.js
@@ -24,28 +24,36 @@ Index.prototype.home = function( req, res ) {
 	});
 };
 
-Index.prototype.login = function( req, res, next ){	
-	var email = req.body('email');
-	var password = req.body('password');
-	if(email == null | email == '') { }
-	else {
-		this._endUsers.authenticate(email, password, function(err, user){
-			if(err) {
-				console.error(err.msg);
-				throw err;
-			}
-			else {
-				req.session.endUser = user;
-				req.session.loggedIn = true;
-			}
-		});
-	}
+Index.prototype.login = function( req, res, next ){		
+	res.locals.title = 'Login';
+	res.locals.error = req.flash('error');
 	res.render('index/login',  { title: "Title of your view", pageName: "home" });	
 };
 
 Index.prototype.loginPost = function( req, res, next ){
-	res.locals.randomVar = "look mom it passed to render";
-	
+	function loginSuccess(result){
+		result.pwd = null;
+		req.session.endUser = result;
+		req.session.endUserLoggedIn = true;
+		if (user.remember) { req.session.cookie.maxAge = 2628000000; }
+		else { req.session.cookie.maxAge = 24*60*60*1000; }
+		res.redirect('/');
+	}
+
+	var email = req.body('email');
+	var password = req.body('password');
+	if(email == null | email == '') { }
+	else {
+		this._endUsers.authenticate(email, password, function(err, result){
+			if (error !== null || !result) {
+				req.session.endUser = {loggedIn:false};
+				req.flash('error', error.message);
+				res.redirect('/login');
+			} else {
+				loginSuccess(result);	
+			}
+		});
+	}
 };
 
 Index.prototype.signup = function( req, res, next ){
